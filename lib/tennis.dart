ee93cbb89858dbf3fe73518a7ebe7c1914a701d3
Cani <anibal0506991@gmail.com>
2023-03-07 13:52:14 -0700

    tennis acabado

--- lib/tennis.dart
+++ lib/tennis.dart
@@ -1,4 +1,82 @@
 
+enum Jugador {p1,p2}
+final String victoria ='Victoria';
+final String advantage ='Advantage';
+final String love = 'Love';
+final String deuce = 'Deuce';
 
+
+class Partido{
+ int _puntosP1 = 0;
+ int _puntosP2 = 0;
+ bool _finalizado =false;
+ 
+ 
+ String get marcador => _marcador();
+
+ String _marcador(){
+  Map<int,String> valores = {
+    0: 'LOVE',
+    1: '15',
+    2: '30',
+    3: '40',
+    
+  };
+
+  if (_recienInicia) {
+    return '${love}';
+  }
+  if (_esEmpatea40omas) {
+    return '${deuce}';
+  }
+  if(_siEmpatadosymasDe40ambos){
+    if (_puntosP1 > _puntosP2) {
+      return '${advantage} 1';
+      
+    }
+    return '${advantage} 2';
+  }
+
+  int diferencia = (_puntosP1 - _puntosP2).abs();
+  {
+    
+    if(_puntosP1 >= 4){ 
+      return '${victoria} 1';   
+    }
+    if (_puntosP2 >= 4) { 
+      return '${victoria} 2';
+    }
+ 
+  }
+
+
+  return '${valores[_puntosP1]}-${valores[_puntosP2]}';
+ }
+
+ bool _esPosibleLaVictoria() {
+  int diferencia = (_puntosP1 - _puntosP2).abs();
+  bool masde40 = (_puntosP1 >= 4) || (_puntosP2 >= 4);
+  return (diferencia > 2) && masde40;
+
+   
+ }
+
+ bool get _siEmpatadosymasDe40ambos => _puntosP1 >= 3 && _puntosP2 >= 3 && _puntosP1 != _puntosP2;
+
+ bool get _recienInicia => _puntosP1 ==0 && _puntosP2 == 0;
+
+ bool get _esEmpatea40omas => _puntosP1 >= 3 && _puntosP2 >= 3 && _puntosP1 == _puntosP2;
+ 
+  void puntoPara(Jugador j) {
+    _finalizado =_esPosibleLaVictoria();
+    if(_finalizado) return;
+    if ( j == Jugador.p1) {
+      _puntosP1 = _puntosP1 + 1;
+      return;
+    }
+    _puntosP2++;
+  }
+ 
+}
  
   
--- test/tennis_test.dart
+++ test/tennis_test.dart
@@ -1,17 +1,21 @@
 import 'package:test/test.dart';
+import 'package:tennis/tennis.dart';
 
 void main() {
 
   late Partido partido;
-  setUp(() => partido =Partido());
+  
+  setUp(() =>
+  partido = Partido()
+  );
 
   test('cuando el partido comienza debe dar 0 - 0 LOVE', (){
-    expect(partido.marcador, equalsIgnoringCase('LOVE'));
+    expect(partido.marcador, equalsIgnoringCase('${love}'));
   });
 
   test('Inicia y el jugador1 anota => 15-LOVE', () {
     partido.puntoPara(Jugador.p1);
-    expect(partido.marcador, equalsIgnoringCase('15-LOVE'));
+    expect(partido.marcador, equalsIgnoringCase('15-${love}'));
     
   });
   test('si van empatados a 15 => 15-15', () {
@@ -28,7 +32,7 @@ void main() {
   partido.puntoPara(Jugador.p1);
   partido.puntoPara(Jugador.p2);
 
-  expect(partido.marcador, equalsIgnoringCase('DEUCE'));
+  expect(partido.marcador, equalsIgnoringCase('${deuce}'));
     
   });
   
@@ -42,86 +46,64 @@ void main() {
   partido.puntoPara(Jugador.p1);
   partido.puntoPara(Jugador.p2);
 
-  expect(partido.marcador, equalsIgnoringCase('DEUCE'));
+  expect(partido.marcador, equalsIgnoringCase(deuce));
     
   });
 
-  test('Si vamos 50-60 => Advantaje P2', () {
-  partido.puntoPara(Jugador.p2);
+  test('Si vamos 50-60 => Advantage P2', () {
   partido.puntoPara(Jugador.p1);
   partido.puntoPara(Jugador.p2);
   partido.puntoPara(Jugador.p1);
   partido.puntoPara(Jugador.p2);
   partido.puntoPara(Jugador.p1);
+  partido.puntoPara(Jugador.p2);
   partido.puntoPara(Jugador.p1);
   partido.puntoPara(Jugador.p2);
   partido.puntoPara(Jugador.p2);    
+
+  expect(partido.marcador, equalsIgnoringCase('${advantage} 2'));
   });
- expect(partido.marcador, equalsIgnoringCase('Advantaje 2'));
+ 
 
  test('si van 40-15 y anota el 1 gana => Victoria 1', () {
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p1);
+  partido.puntoPara(Jugador.p2);
+
+  expect(partido.marcador, equalsIgnoringCase('${victoria} 2'));
+   
+ });
+
+ test('misterio', () {
+  partido.puntoPara(Jugador.p1);
+  partido.puntoPara(Jugador.p1);
+  partido.puntoPara(Jugador.p1);
+  partido.puntoPara(Jugador.p1);
   partido.puntoPara(Jugador.p1);
   partido.puntoPara(Jugador.p1);
   partido.puntoPara(Jugador.p1);
   partido.puntoPara(Jugador.p2);
-  partido.puntoPara(Jugador.p1);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
+  partido.puntoPara(Jugador.p2);
 
-  expect(partido.marcador, equalsIgnoringCase('Victoria 1'));
-   
+
+ expect(partido.marcador, equalsIgnoringCase('${victoria} 1'));
  });
 
 }
 
 
-enum Jugador {p1,p2}
-
-class Partido{
- int _puntosP1 = 0;
- int _puntosP2 = 0;
- 
- 
- String get marcador => _marcador();
-
- String _marcador(){
-  Map<int,String> valores = {
-    0: 'LOVE',
-    1: '15',
-    2: '30',
-    3: '40',
-  };
-
-  if (_puntosP1 ==0 && _puntosP2 == 0 ) {
-    return 'LOVE';
-  }
-  if (_puntosP1 >= 3 && _puntosP2 >= 3 && _puntosP1 == _puntosP2) {
-    return 'DEUCE';
-  }
-  if(_puntosP1 > 3 && _puntosP2 > 3 && _puntosP1 != _puntosP2){
-    if (_puntosP1 > _puntosP2) {
-      return 'Advantaje 1';
-      
-    }
-    return 'Advantaje 2';
-  }
-  int diferencia =(_puntosP1 - _puntosP2.abs());
-  if(diferencia == 2 ){
-    if(_puntosP1 > 4){
-        return 'Victoria 1';
-    }
-    if (_puntosP2 > 4) {
-      return 'Victoria 2';
-      
-    }
-  }
-  return '${valores[_puntosP1]}-${valores[_puntosP2]}';
- }
- 
-  void puntoPara(Jugador j) {
-    if ( j == Jugador.p1) {
-      _puntosP1 = _puntosP1 + 1;
-      return;
-    }
-    _puntosP2++;
-  }
-
-}
